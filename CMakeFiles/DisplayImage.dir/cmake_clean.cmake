923243c55c8e8882be750e2b5f8fa34623fad5b0
Littlewhite <964873559@qq.com>
2016-06-25 12:22:51 +0800

    OPENCV配置完成

---
+++ CMakeFiles/DisplayImage.dir/cmake_clean.cmake
@@ -0,0 +1,9 @@
+file(REMOVE_RECURSE
+  "DisplayImage.pdb"
+  "DisplayImage"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/DisplayImage.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
