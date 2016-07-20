ae19cfb69cb3372669885e10cfe5477a80009c98
Littlewhite <964873559@qq.com>
2016-07-20 17:18:56 +0800

    切出每一个字

--- Cut/touying.cpp
+++ Cut/touying.cpp
@@ -6,50 +6,95 @@
 
 using namespace cv;
 
+int* projectH(IplImage* src);
+
+int* projectW(IplImage* src);
+
+void cutH(int* h,IplImage* src);
+
+void cutW(int* h,IplImage* src);
+
+//计数器
+int count = 0;
 
 int main(int argc, char* argv[])
 {
     //    加载图像
     IplImage* imgSrc = cvLoadImage(IMAGE,CV_LOAD_IMAGE_COLOR);
+    
     //    将图像转换为灰度图
     IplImage* img_gray = cvCreateImage(cvGetSize(imgSrc), IPL_DEPTH_8U, 1);
     cvCvtColor(imgSrc, img_gray, CV_BGR2GRAY);
+    
     //    二值化处理
     cvThreshold(img_gray, img_gray,100, 255,CV_THRESH_BINARY_INV);// CV_THRESH_BINARY_INV使得背景为黑色，字符为白色，这样找到的最外层才是字符的最外层
     cvShowImage("ThresholdImg",img_gray);
+    
+//    执行投影操作
+    int *h = projectH(img_gray);
+    
+//    将图片切割分行
+    cutH(h,img_gray);
+    
+    printf("找到的汉字数量为：%d",count);
+    //显示
+    cvWaitKey(0);
+    cvReleaseImage(&imgSrc);
+    cvReleaseImage(&img_gray);
+    cvDestroyAllWindows();
+    return 0;  
+}
+
+
+int* projectH(IplImage* src) {
     //存储投影值的数组
-    int* h = new int[img_gray->height];
-    for(int y = 0;y < img_gray->height;y++) {
+    int* h = new int[src->height];
+    for(int y = 0;y < src->height;y++) {
         h[y] = 0;
     }
     //对每一行计算投影值
-    for(int y = 0;y < img_gray->height;y++)
+    for(int y = 0;y < src->height;y++)
     {
-        uchar* ptr = (uchar*)(img_gray->imageData + y*img_gray->widthStep);
+        uchar* ptr = (uchar*)(src->imageData + y*src->widthStep);
         //遍历这一行的每一个像素，如果是有效的，累加投影值
-        for(int x = 0;x < img_gray->width;x++)
+        for(int x = 0;x < src->width;x++)
         {
             if(ptr[x] == 255)
                 h[y]++;
         }
     }
     
-    //准备一个图像用于画投影图
-    IplImage* paintY = cvCreateImage(cvGetSize(img_gray), IPL_DEPTH_8U, 1);
-    cvZero(paintY);
-    
-    //画图
-//    CvScalar t= cvScalar(255,0,0,0);
-//    是否进入投影区
+    return h;
+}
+
+int* projectW(IplImage* src) {
+    //存储投影值的数组
+    int* w = new int[src->width];
+    for(int x = 0;x < src->width;x++) {
+        w[x] = 0;
+    }
+    //对每一列计算投影值
+    for(int y = 0;y < src->height;y++)
+    {
+        uchar* ptr = (uchar*)(src->imageData + y*src->widthStep);
+        //遍历这一行的每一个像素，如果是有效的，累加投影值
+        for(int x = 0;x < src->width;x++)
+        {
+            if(ptr[x] == 255)
+                w[x]++;
+        }
+    }
+    return w;
+}
+
+void cutH(int* h,IplImage* src) {
     bool inWord = false;
     //分割开始位置和结束位置
     int start = 0,end = 0;
-//    char szName[56] = {0};
-//    获得行数
-    int HEIGHT = paintY->height;
-//    存储每一行的图片
-    IplImage* imgNo[HEIGHT];
-    int i = 0;
+    //    获得行数
+    int HEIGHT = src->height;
+    //    存储每一行的图片
+    IplImage* imgNo;
     //存储
     for(int y = 0;y < HEIGHT;y++)
     {
@@ -59,86 +104,47 @@ int main(int argc, char* argv[])
         }else if(inWord && h[y] == 0) {
             inWord = false;
             end = y;
-            imgNo[i] = cvCreateImage(cvSize(img_gray->width,end-start+1), IPL_DEPTH_8U, 1);
-            cvSetImageROI(img_gray, cvRect(0, start, imgNo[i]->width, imgNo[i]->height));
-            cvCopyImage(img_gray, imgNo[i]);
-            cvResetImageROI(img_gray);
-            i++;
-//            sprintf(szName, "windowstart_%d", start);
-//            cvNamedWindow(szName);
-//            cvShowImage(szName, imgNo[y]);
-//            cvReleaseImage(&imgNo[y]);
-        }
-//        printf("%d\n",h[y]);
-    }
-    
-    
-//    准备对列进行投影
-    //存储投影值的数组
-    int* w = new int[imgNo[0]->width];
-    for(int x = 0;x < imgNo[0]->width;x++) {
-        w[x] = 0;
-    }
-    //对每一列计算投影值
-    for(int y = 0;y < imgNo[0]->height;y++)
-    {
-        uchar* ptr = (uchar*)(imgNo[0]->imageData + y*imgNo[0]->widthStep);
-        //遍历这一行的每一个像素，如果是有效的，累加投影值
-        for(int x = 0;x < img_gray->width;x++)
-        {
-            if(ptr[x] == 255)
-                w[x]++;
+            imgNo = cvCreateImage(cvSize(src->width,end-start+1), IPL_DEPTH_8U, 1);
+            cvSetImageROI(src, cvRect(0, start, imgNo->width, imgNo->height));
+            cvCopyImage(src, imgNo);
+            cvResetImageROI(src);
+            //对每一行的图像进行列投影处理
+            int* w = projectW(imgNo);
+            cutW(w,imgNo);
         }
     }
-    
-    //准备一个图像用于画投影图
-    IplImage* paintX = cvCreateImage(cvGetSize(imgNo[0]), IPL_DEPTH_8U, 1);
-    cvZero(paintX);
-    
-    //画图
-    CvScalar t= cvScalar(255,0,0,0);
-    //    是否进入投影区
-    inWord = false;
+
+}
+
+void cutW(int* w,IplImage* src) {
+    bool inWord = false;
     //分割开始位置和结束位置
-    start = 0,end = 0;
-    char szName[56] = {0};
+    int start = 0,end = 0;
+    //    获得列数
+    int WIDTH = src->width;
+    //    存储每一列的图片
+    IplImage* imgNo;
+    char szName[100] = {0};
     //存储
-    for(int x = 0;x < imgNo[0]->width;x++)
+    for(int x = 0;x < WIDTH;x++)
     {
-        for(int y = 0;y < w[x];y++)
-            cvSet2D(paintX, y, x, t);
-    }
-    
-    //进行切割
-    for(int x = 0;x < imgNo[0]->width;x++) {
-        if(!inWord && w[x]>0) {
-            start = x;
+        if(!inWord && w[x] > 0) {
             inWord = true;
-        } else if(inWord && w[x] == 0) {
-            end = x;
+            start = x;
+        }else if(inWord && w[x] == 0) {
             inWord = false;
-            IplImage* imgNo2 = cvCreateImage(cvSize(end-start+1,imgNo[0]->height), IPL_DEPTH_8U, 1);
-            cvSetImageROI(img_gray, cvRect(start, 0, imgNo2->width, imgNo2->height));
-            cvCopyImage(img_gray, imgNo2);
-            cvResetImageROI(img_gray);
+            end = x;
+            imgNo = cvCreateImage(cvSize(end-start+1,src->height), IPL_DEPTH_8U, 1);
+            cvSetImageROI(src, cvRect(start-1, 0, imgNo->width, imgNo->height));
+            cvCopyImage(src, imgNo);
+            cvResetImageROI(src);
+//            显示每一行的结果
             sprintf(szName, "windowstart_%d", start);
             cvNamedWindow(szName);
-            cvShowImage(szName, imgNo2);
-            cvReleaseImage(&imgNo2);
-
+            cvShowImage(szName, imgNo);
+            cvReleaseImage(&imgNo);
+            count++;
         }
     }
-    
-    cvNamedWindow("imgNo[0]");
-    cvShowImage("imgNo[0]", imgNo[0]);
-    //显示
-//    cvNamedWindow("PaintX");
-//    cvShowImage("PaintX", paintX);
-    cvWaitKey(0);
-    cvReleaseImage(&imgSrc);
-    cvReleaseImage(&img_gray);
-    cvReleaseImage(&imgNo[0]);
-//    cvReleaseImage(&paintY);
-    cvDestroyAllWindows();
-    return 0;  
+
 }
