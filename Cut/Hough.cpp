3abea4a88318295cd23e14b786d435d3d8a1f505
Littlewhite <964873559@qq.com>
2016-08-02 22:16:00 +0800

    霍夫直线变换

--- Cut/Hough.cpp
+++ Cut/Hough.cpp
@@ -6,42 +6,60 @@
 
 int main(int argc, char **argv)
 {
-    //    加载灰度图像
-    IplImage* image = cvLoadImage(
-                                  IMAGE,
-                                  CV_LOAD_IMAGE_GRAYSCALE
-                                  );
-    //    变成更好看的彩色
-    IplImage* src = cvLoadImage( IMAGE ); //Changed for prettier show in color
-    //    创建存储区
+    IplImage* src = cvLoadImage( IMAGE, 0 );
+    IplImage* dst;
+    IplImage* color_dst;
     CvMemStorage* storage = cvCreateMemStorage(0);
-    //    模糊处理，输入输出、模糊方法、水平和竖直方向sigma值
-    cvSmooth(image, image, CV_GAUSSIAN, 5, 5 );
-    //    进行霍夫圆变换
-    CvSeq* results = cvHoughCircles(
-                                    image,
-                                    storage,
-                                    CV_HOUGH_GRADIENT,
-                                    4,
-                                    image->width/10
-                                    );
-    //    遍历结果集
-    for( int i = 0; i < results->total; i++ ) {
-        //      返回索引制定的元素指针
-        float* p = (float*) cvGetSeqElem( results, i );
-        //      创建圆心
-        CvPoint pt = cvPoint( cvRound( p[0] ), cvRound( p[1] ) );
-        //      绘制圆
-        cvCircle(
-                 src,
-                 pt, 
-                 cvRound( p[2] ),
-                 CV_RGB(0xff,0,0) 
-                 );
+    CvSeq* lines = 0;
+    int i;
+    
+    if( !src )
+    {
+        printf("Load image error!\n");
+        return -1;
     }
-    //    显示结果
-    cvNamedWindow( "cvHoughCircles", 1 );
-    cvShowImage( "cvHoughCircles", src);
+    
+    dst = cvCreateImage( cvGetSize(src), 8, 1 );
+    color_dst = cvCreateImage( cvGetSize(src), 8, 3 );
+    
+    cvCanny( src, dst, 50, 200, 3 );
+    //cvThreshold(src, dst, 50, 255, CV_THRESH_BINARY_INV);
+    
+    cvCvtColor( dst, color_dst, CV_GRAY2BGR );
+#if 0   //标准hough变换
+    printf("标准hough变换\n");
+    lines = cvHoughLines2( dst, storage, CV_HOUGH_STANDARD, 1, CV_PI/180, 100, 0, 0 );
+    
+    for( i = 0; i < MIN(lines->total,100); i++ )
+    {
+        float* line = (float*)cvGetSeqElem(lines,i);
+        float rho = line[0];
+        float theta = line[1];
+        CvPoint pt1, pt2;
+        double a = cos(theta), b = sin(theta);
+        double x0 = a*rho, y0 = b*rho;
+        pt1.x = cvRound(x0 + 1000*(-b));
+        pt1.y = cvRound(y0 + 1000*(a));
+        pt2.x = cvRound(x0 - 1000*(-b));
+        pt2.y = cvRound(y0 - 1000*(a));
+        cvLine( color_dst, pt1, pt2, CV_RGB(255,0,0), 3, CV_AA, 0 );
+    }
+#else   //累积概率hough变换
+    printf("累积概率hough变换\n");
+    lines = cvHoughLines2( dst, storage, CV_HOUGH_PROBABILISTIC, 1, CV_PI/180, 50, 50, 10 );
+    for( i = 0; i < lines->total; i++ )
+    {
+        CvPoint* line = (CvPoint*)cvGetSeqElem(lines,i);
+        cvLine( color_dst, line[0], line[1], CV_RGB(255,0,0), 3, CV_AA, 0 );
+    }
+#endif
+    cvNamedWindow( "Source", 1 );
+    cvShowImage( "Source", src );
+    
+    cvNamedWindow( "Hough", 1 );
+    cvShowImage( "Hough", color_dst );
+    
     cvWaitKey(0);
-
+    
+    return 0;
 }
